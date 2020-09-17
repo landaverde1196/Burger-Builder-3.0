38b0e100cb88bb8453b3fc97c1caeaac9d4d055b
landaverde1196 <alejndro1196@gmail.com>
2020-09-17 16:17:37 -0600

    make it better

--- src/server.js
+++ src/server.js
@@ -1,19 +1,13 @@
 const path = require("path");
 
-const express = require("express");
+let express = require("express");
+let app = express();
+app.use(express.static(__dirname + "/"));
+app.listen(process.env.PORT || 3000);
 
-const app = express();
-
-const publicPath = path.join(__dirname, "..", "public");
-
-const port = process.env.PORT || 3000;
-
-app.use(express.static(publicPath));
-
-app.get("*", (req, res) => {
-  res.sendFile(path.join(publicPath, "index.html"));
-});
-
-app.listen(port, () => {
-  console.log(`Server is up on port ${port}!`);
-});
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("build"));
+  app.get("*", (req, res) => {
+    res.sendFile(path.join("build", "index.html"));
+  });
+}
